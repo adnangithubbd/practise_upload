78ea1e8601288ab61f531810d93de49a7d6fa128
adnan_habib <adnanhabib9277@gmail.com>
2023-11-07 02:00:26 +0600

    lots of update for you !!

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,6 @@ void main() {
   ));
 }
 
-
-
-
 Widget inputArea() {
   return Column(
     mainAxisSize: MainAxisSize.min,
@@ -48,8 +45,6 @@ Widget inputArea() {
   );
 }
 
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -58,7 +53,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -69,7 +63,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -82,14 +75,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
